10c55ed7771174765dd7c0e60ac4ae1ddd7f7c89
Leblanc Meneses <leblanc@robusthaven.com>
2013-12-18 17:07:27 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WriteAssemblyBinding
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var assemblies = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.dll").Select(s => Assembly.LoadFile(s)).ToList();
+
+			var template = new WriteBindingTemplate();
+			template.Model = assemblies;
+			File.WriteAllText(Path.Combine(Environment.CurrentDirectory, "assemblyBinding.xml"), template.TransformText());
+		}
+	}
+}
